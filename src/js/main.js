4debfe196bdcb034a5e70b32dae7f1b1f7932ba7
Sanjeevan Rames <thesilverghost2005@gmail.com>
2025-10-03 11:26:40 +0800

    made grid

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+const container = document.querySelector("div");
+const grid = document.createElement("div");
+const row = document.createElement("div");
+row.textContent = "test 1";
+grid.textContent = "test 2";
+
+for (let i = 0;i < 16;i++){
+	row.appendChild(grid.cloneNode(true));
+}
+
+for (let i = 0;i < 16;i++){
+	container.appendChild(row.cloneNode(true));
+}
